ba1ba29a31af6dd3e9612f15d826b8a22c86731a
amaan (aman) khan <khan.amaan2k6@gmail.com>
2025-05-02 19:49:57 +0530

    Add files via upload

---
+++ SwitchCalc.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class SwitchCalc {
+   public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);// Scanner object for input
+
+    // Asking user to enter two numbers
+    System.out.println("please enter two numbers"); 
+      int a = scn.nextInt();
+      int b = scn.nextInt();
+      
+      // Asking user which operation they want to perform
+   System.out.println("please enter which type of calcultaion you want to perform");
+         char c = scn.next().charAt(0); // Taking character input for operation
+
+
+         // Using switch case to perform the chosen operation
+         switch (c) {
+               case '+' :
+               System.out.println(a + b);
+               break ;
+               case '-' :
+               System.out.println(a - b);
+               break ;
+               case '*' :
+               System.out.println(a * b);
+               break ;
+               case '/' : 
+               System.out.println(a / b);
+               break;
+               case '%' :
+                  System.out.println(a % b);
+                  break;
+                  
+                  default : // If user enters any other character
+                  System.out.println("you entered wrong number , please write between 1 to 5");
+
+
+         }
+      }
+   }
+
